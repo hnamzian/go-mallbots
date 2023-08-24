2c23cebe3183752c9a707a04da7fa2d80df21f49
hnamzian <namazian87@gmail.com>
2023-08-24 14:50:50 -0400

    Wrapped app context with ergroup to run servers in parallel + cancel context at os/syscall signals

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/hnamzian/go-mallbots/internal/config"
 	"github.com/hnamzian/go-mallbots/internal/logger"
@@ -15,9 +19,9 @@ func main() {
 
 func run() error {
 	cfg, err := config.InitConfig()
-	if err!= nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 	app := &App{
 		cfg: cfg,
 	}
@@ -31,7 +35,21 @@ func run() error {
 	defer app.closeDB()
 
 	ctx := context.Background()
-	app.waitForWebServer(ctx)
+	ctx, cancel := context.WithCancel(ctx)
+	ctx, cancel = signal.NotifyContext(ctx, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	return nil
+	group, gCtx := errgroup.WithContext(ctx)
+	group.Go(func() error {
+		<-gCtx.Done()
+		cancel()
+		return nil
+	})
+	group.Go(func() error {
+		return app.waitForWebServer(gCtx)
+	})
+	group.Go(func() error {
+		return app.waitForRPC(gCtx)
+	})
+
+	return group.Wait()
 }
