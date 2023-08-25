7b3881d0f1dcc4008b3d729faad3fc4d69db251b
hnamzian <namazian87@gmail.com>
2023-08-25 13:26:27 -0400

    Initiated application at Startup

--- customers/module.go
+++ customers/module.go
@@ -3,14 +3,20 @@ package customers
 import (
 	"context"
 
-	"github.com/hnamzian/go-mallbots/internal/module"
+	"github.com/hnamzian/go-mallbots/customers/internal/application"
 	"github.com/hnamzian/go-mallbots/customers/internal/grpc"
+	"github.com/hnamzian/go-mallbots/customers/internal/repository"
+	"github.com/hnamzian/go-mallbots/internal/module"
 )
 
 type Module struct {}
 
 func (m Module) Startup(ctx context.Context, app module.Core) error{
-	grpc.RegisterServer(app.RPC(), grpc.Server{})
+	customers := repository.NewCustomersRepository("customers.customers", app.DB())
+
+	customersApp := application.NewApplication(customers)
+
+	grpc.RegisterServer(app.RPC(), customersApp)
 
 	return nil
 }
