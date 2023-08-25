3f669feee89f1ba1ce62a9650d210195974e9255
hnamzian <namazian87@gmail.com>
2023-08-25 11:30:49 -0400

    Removed customers entity and replaced it by domain customer

--- customers/internal/repository/customers_entity.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-type Customer struct {
-	ID        string `json:"id"`
-	Name      string `json:"name"`
-	SmsNumber string `json:"sms_number"`
-	Enabled   bool   `json:"enabled"`
-}
