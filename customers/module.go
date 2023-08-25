1dd8187affac5c78caeb99a44c261066e044f9ac
hnamzian <namazian87@gmail.com>
2023-08-25 19:49:29 -0400

    Registered rest api to grpc gateway + mounted swagger

--- customers/module.go
+++ customers/module.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/hnamzian/go-mallbots/customers/internal/application"
 	"github.com/hnamzian/go-mallbots/customers/internal/grpc"
-	"github.com/hnamzian/go-mallbots/customers/internal/repository"
 	"github.com/hnamzian/go-mallbots/customers/internal/logger"
+	"github.com/hnamzian/go-mallbots/customers/internal/repository"
+	"github.com/hnamzian/go-mallbots/customers/internal/rest"
 	"github.com/hnamzian/go-mallbots/internal/module"
 )
 
-type Module struct {}
+type Module struct{}
 
-func (m Module) Startup(ctx context.Context, core module.Core) error{
+func (m Module) Startup(ctx context.Context, core module.Core) error {
 	customers := repository.NewCustomersRepository("customers.customers", core.DB())
 
 	var app application.App
@@ -20,6 +21,13 @@ func (m Module) Startup(ctx context.Context, core module.Core) error{
 	app = logger.NewApplication(app, core.Logger())
 
 	grpc.RegisterServer(core.RPC(), app)
+
+	if err := rest.RegisterGateway(ctx, core.Mux(), core.Config().Grpc.Address()); err != nil {
+		return err
+	}
+	if err := rest.RegisterSwagger(core.Mux()); err != nil {
+		return err
+	}
 
 	return nil
 }
