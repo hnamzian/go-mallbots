4786697bf8f840a92fb59fd4f481b73c3d6df9d2
hnamzian <namazian87@gmail.com>
2023-08-25 11:52:32 -0400

    Used application implement grpc endpoints

--- customers/internal/grpc/server.go
+++ customers/internal/grpc/server.go
@@ -3,11 +3,14 @@ package grpc
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/hnamzian/go-mallbots/customers/customerspb"
+	"github.com/hnamzian/go-mallbots/customers/internal/application"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
+	app application.App
 	customerspb.UnimplementedCustomersServer
 }
 
@@ -16,17 +19,49 @@ func RegisterServer(registrar grpc.ServiceRegistrar, srv Server) {
 }
 
 func (s Server) RegisterCustomer(ctx context.Context, request *customerspb.RegisterCustomerRequest) (*customerspb.RegisterCustomerResponse, error) {
-	return nil, nil
+	id := uuid.New().String()
+	err := s.app.RegisterCustomer(ctx, application.RegisterCustomer{
+		ID:        id,
+		Name:      request.Name,
+		SmsNumber: request.SmsNumber,
+	})
+	if err!= nil {
+		return nil, err
+    }
+	return &customerspb.RegisterCustomerResponse{Id: id}, nil
 }
 
 func (s Server) GetCustomers(ctx context.Context, request *customerspb.GetCustomerRequest) (*customerspb.GetCustomerResponse, error) {
-	return nil, nil
+	customer, err := s.app.GetCustomer(ctx, application.GetCustomer{
+		ID: request.Id,
+    })
+	if err!= nil {
+        return nil, err
+    }
+	return &customerspb.GetCustomerResponse{Customer: &customerspb.Customer{
+		Id: customer.ID,
+        Name: customer.Name,
+        SmsNumber: customer.SmsNumber,
+		Enabled: customer.Enabled,
+	}}, nil
 }
 
 func (s Server) EnableCustomer(ctx context.Context, request *customerspb.EnableCustomerRequest) (*customerspb.EnableCustomerResponse, error) {
-	return nil, nil
+	err := s.app.EnableCustomer(ctx, application.EnableCustomer{
+		ID: request.Id,
+    })
+    if err!= nil {
+		return nil, err	
+	}
+	return &customerspb.EnableCustomerResponse{}, nil
 }
 
 func (s Server) DisableCustomer(ctx context.Context, request *customerspb.DisableCustomerRequest) (*customerspb.DisableCustomerResponse, error) {
-	return nil, nil
+	err := s.app.DisableCustomer(ctx, application.DisableCustomer{
+		ID: request.Id,
+    })
+    if err!= nil {
+		return nil, err	
+	}
+	return &customerspb.DisableCustomerResponse{}, nil
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.1
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
