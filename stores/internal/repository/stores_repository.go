8bff812b99ae059a9ed5393ec0bdea83014c1199
hnamzian <namazian87@gmail.com>
2023-08-28 22:27:27 -0400

    Implementing interface instead of pointer of struct

--- stores/internal/repository/stores_repository.go
+++ stores/internal/repository/stores_repository.go
@@ -3,8 +3,9 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"github.com/stackus/errors"
 	"fmt"
+
+	"github.com/stackus/errors"
 
 	"github.com/hnamzian/go-mallbots/stores/internal/domain"
 )
@@ -14,14 +15,14 @@ type StoreRepository struct {
 	db        *sql.DB
 }
 
-func NewStore(tableName string, db *sql.DB) *StoreRepository {
+func NewStoreRepository(tableName string, db *sql.DB) *StoreRepository {
 	return &StoreRepository{
 		tableName: tableName,
 		db:        db,
 	}
 }
 
-func (r StoreRepository) Save(ctx context.Context, store *domain.Store) error {
+func (r *StoreRepository) Save(ctx context.Context, store *domain.Store) error {
 	query := fmt.Sprintf("INSERT INTO %s (id, name, location, participating) VALUES ($1, $2, $3, $4)", r.tableName)
 
 	_, err := r.db.ExecContext(ctx, query, store.ID, store.Name, store.Location, store.Participating)
@@ -32,7 +33,7 @@ func (r StoreRepository) Save(ctx context.Context, store *domain.Store) error {
 	return nil
 }
 
-func (r StoreRepository) Get(ctx context.Context, id string) (*domain.Store, error) {
+func (r *StoreRepository) Get(ctx context.Context, id string) (*domain.Store, error) {
 	query := fmt.Sprintf("SELECT id, name, location, participating FROM %s WHERE id = $1", r.tableName)
 
 	store := &domain.Store{}
@@ -43,27 +44,26 @@ func (r StoreRepository) Get(ctx context.Context, id string) (*domain.Store, err
 	return store, nil
 }
 
-func (r StoreRepository) GetAll(ctx context.Context) ([]*domain.Store, error) {
+func (r *StoreRepository) GetAll(ctx context.Context) ([]*domain.Store, error) {
 	query := fmt.Sprintf("SELECT id, name, location, participating FROM %s ORDER BY id ASC", r.tableName)
 
 	rows, err := r.db.QueryContext(ctx, query)
-	if err!= nil {
-        return nil, errors.Wrap(err, "failed to find stores")
-    }
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find stores")
+	}
 
 	defer func(rows *sql.Rows) {
 		if err := rows.Close(); err != nil {
 			err = errors.Wrap(err, "failed to close rows")
-        }
+		}
 	}(rows)
 
 	stores := []*domain.Store{}
 	for rows.Next() {
-		var store *domain.Store
-
-		if err = rows.Scan(&store.ID, &store.Name, &store.Location, &store.Participating); err!= nil {
+		store := &domain.Store{}
+		if err = rows.Scan(&store.ID, &store.Name, &store.Location, &store.Participating); err != nil {
 			return nil, errors.Wrap(err, "failed to scan store")
-        }
+		}
 
 		stores = append(stores, store)
 	}
@@ -75,29 +75,29 @@ func (r StoreRepository) GetAll(ctx context.Context) ([]*domain.Store, error) {
 	return stores, nil
 }
 
-func (r StoreRepository) GetParticipatingStore(ctx context.Context, store *domain.Store) ([]*domain.Store ,error) {
+func (r *StoreRepository) GetParticipatingStores(ctx context.Context, store *domain.Store) ([]*domain.Store, error) {
 	query := fmt.Sprintf("SELECT id, name, location, participating FROM %s WHERE participating is true", r.tableName)
 
 	rows, err := r.db.QueryContext(ctx, query)
-	if err!= nil {
-        return nil, errors.Wrap(err, "failed to find participating stores")
-    }
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find participating stores")
+	}
 
 	defer func(rows *sql.Rows) {
-        if err := rows.Close(); err!= nil {
-            err = errors.Wrap(err, "failed to close rows")
-        }
-    }(rows)
+		if err := rows.Close(); err != nil {
+			err = errors.Wrap(err, "failed to close rows")
+		}
+	}(rows)
 
 	stores := []*domain.Store{}
 	for rows.Next() {
-		var store *domain.Store
+		store := &domain.Store{}
 
-        if err = rows.Scan(&store.ID, &store.Name, &store.Location, &store.Participating); err!= nil {
-            return nil, errors.Wrap(err, "failed to scan store")
-        }
+		if err = rows.Scan(&store.ID, &store.Name, &store.Location, &store.Participating); err != nil {
+			return nil, errors.Wrap(err, "failed to scan store")
+		}
 
-        stores = append(stores, store)
+		stores = append(stores, store)
 	}
 
 	if err = rows.Err(); err != nil {
@@ -105,26 +105,26 @@ func (r StoreRepository) GetParticipatingStore(ctx context.Context, store *domai
 	}
 
 	return stores, nil
-} 
+}
 
-func (r StoreRepository) Update(ctx context.Context, store *domain.Store) error {
+func (r *StoreRepository) Update(ctx context.Context, store *domain.Store) error {
 	query := fmt.Sprintf("UPDATE %s SET name = $2, location = $3, participating = $3 WHERE id = $1", r.tableName)
 
 	_, err := r.db.ExecContext(ctx, query, store.ID, store.Name, store.Location, store.Participating)
-	if err!= nil {
-        return errors.Wrap(err, "failed to update store")
-    }
+	if err != nil {
+		return errors.Wrap(err, "failed to update store")
+	}
 
 	return nil
 }
 
-func (r StoreRepository) Delete(ctx context.Context, store *domain.Store) error {
+func (r *StoreRepository) Delete(ctx context.Context, id string) error {
 	query := fmt.Sprintf("DELETE FROM %s WHERE id = $1", r.tableName)
 
-    _, err := r.db.ExecContext(ctx, query, store.ID)
-    if err!= nil {
-        return errors.Wrap(err, "failed to delete store")
-    }
+	_, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return errors.Wrap(err, "failed to delete store")
+	}
 
-    return nil
+	return nil
 }
