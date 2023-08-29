06c4a317ab26650ed706cd364391631c7870ee96
hnamzian <namazian87@gmail.com>
2023-08-28 22:28:29 -0400

    Store module implemented

---
+++ stores/module.go
@@ -0,0 +1,27 @@
+package stores
+
+import (
+	"context"
+
+	"github.com/hnamzian/go-mallbots/internal/module"
+	"github.com/hnamzian/go-mallbots/stores/internal/application"
+	"github.com/hnamzian/go-mallbots/stores/internal/grpc"
+	"github.com/hnamzian/go-mallbots/stores/internal/repository"
+	"github.com/hnamzian/go-mallbots/stores/internal/logger"
+)
+
+type Module struct{}
+
+func (m Module) Startup(ctx context.Context, core module.Core) error {
+	var app application.App
+	storeRepository := repository.NewStoreRepository("stores.stores", core.DB())
+	participatingStoreRepository := repository.NewParticipatingStoreRepository("stores.stores", core.DB())
+	productRepository := repository.NewProductRepository("stores.products", core.DB())
+	app = application.NewApplication(storeRepository, productRepository, participatingStoreRepository)
+	
+	app = logger.NewApplication(app, core.Logger())
+
+	grpc.RegisterServer(core.RPC(), app)
+
+	return nil
+}
