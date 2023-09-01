458f2e793cc36d595be005e304d2303a08369750
hnamzian <namazian87@gmail.com>
2023-09-01 18:49:30 -0400

    Started payment module and registerd grpc server

---
+++ payments/module.go
@@ -0,0 +1,32 @@
+package payments
+
+import (
+	"context"
+
+	"github.com/hnamzian/go-mallbots/payments/internal/application"
+	"github.com/hnamzian/go-mallbots/payments/internal/repository"
+	"github.com/hnamzian/go-mallbots/payments/internal/logger"
+	"github.com/hnamzian/go-mallbots/payments/internal/grpc"
+	"github.com/hnamzian/go-mallbots/internal/module"
+)
+
+type Module struct {}
+
+func (m Module) Startup(ctx context.Context, core module.Core) error {
+	invoices := repository.NewInvoiceRepository(core.DB())
+	payments := repository.NewPaymentRepository(core.DB())
+
+	conn, err := grpc.Dial(ctx, core.Config().Grpc.Address())
+	if err != nil {
+		return err
+	}
+	orders := grpc.NewOrderRepository(conn)
+	
+	var app application.App
+	app = application.NewApplication(invoices, payments, orders)
+	app = logger.NewApplication(core.Logger(), app)
+
+	grpc.RegisterServer(core.RPC(), app)
+	
+	return nil
+}
