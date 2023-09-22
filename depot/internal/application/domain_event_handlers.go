4008940cd5fca0073367fe80d862b011d684c142
hnamzian <namazian87@gmail.com>
2023-09-22 14:45:00 -0400

    Added base of shoppingList DomainHandlers of handlers

---
+++ depot/internal/application/domain_event_handlers.go
@@ -0,0 +1,34 @@
+package application
+
+import (
+	"context"
+
+	"github.com/hnamzian/go-mallbots/internal/ddd"
+)
+
+type DomainHandlers interface {
+	OnShoppingListCreated(context.Context, ddd.Event) error
+	OnShoppingListCancelled(context.Context, ddd.Event) error
+	OnShoppingListAssigned(context.Context, ddd.Event) error
+	OnShoppingListCompleted(context.Context, ddd.Event) error
+}
+
+type ignoreUnimplementedDomainHandlers struct{}
+
+var _ DomainHandlers = (*ignoreUnimplementedDomainHandlers)(nil)
+
+func (ignoreUnimplementedDomainHandlers) OnShoppingListCreated(context.Context, ddd.Event) error {
+	return nil
+}
+
+func (ignoreUnimplementedDomainHandlers) OnShoppingListCancelled(context.Context, ddd.Event) error {
+	return nil
+}
+
+func (ignoreUnimplementedDomainHandlers) OnShoppingListAssigned(context.Context, ddd.Event) error {
+	return nil
+}
+
+func (ignoreUnimplementedDomainHandlers) OnShoppingListCompleted(context.Context, ddd.Event) error {
+	return nil
+}
