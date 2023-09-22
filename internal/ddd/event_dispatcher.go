2ff4a0c4ab913163465178f56b977e5318a8f130
hnamzian <namazian87@gmail.com>
2023-09-22 07:08:54 -0400

    Added Aggregate and event dispatcher as base of domain event pub/sub

---
+++ internal/ddd/event_dispatcher.go
@@ -0,0 +1,47 @@
+package ddd
+
+import (
+	"context"
+	"sync"
+)
+
+type EventSubscriber interface {
+	Subscribe(event Event, handler EventHandler)
+}
+
+type EventPublisher interface {
+	Publish(ctx context.Context, event Event) error
+}
+
+type EventDispatcher struct {
+	mu sync.RWMutex
+	handlers map[string][]EventHandler
+}
+
+var _ interface {
+	EventSubscriber
+	EventPublisher
+} = (*EventDispatcher)(nil)
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandler),
+	}
+}
+
+func (d *EventDispatcher) Subscribe(event Event, handler EventHandler) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.handlers[event.EventName()] = append(d.handlers[event.EventName()], handler)
+}
+
+func (d *EventDispatcher) Publish(ctx context.Context, event Event) error {
+	for _, handler := range d.handlers[event.EventName()] {
+		if err := handler(ctx, event); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
